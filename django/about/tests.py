f03ab8484364ecd4ec03c926589fd9623c5bd2ef
Steven Young <ywch2002@126.com>
2023-05-22 17:33:03 +0800

    remove redundancy

--- django/about/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class TestAboutViews(TestCase):
-
-    def test_about_page(self):
-        response = self.client.get("/about/")
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('about.html')
--- django/about/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def about_view(request):
-    return render(request, 'about.html')
-
-# Create your views here.
